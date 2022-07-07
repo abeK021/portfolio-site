964e0ad83c931c19e9cd87412202a5e1a67e13df
abeakaid <74392388+abeK021@users.noreply.github.com>
2022-07-07 18:12:45 -0400

    Fixed scrolling no moving on to about

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
 // React stuff
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 // COMPONENTS
-import Heading from "./components/heading/heading.jsx";
-import ScrollableTabs from "./components/nav-tabs/nav-tabs.jsx";
-import Hero from "./components/hero/hero.jsx";
+import Heading from "./components/1-heading/heading.jsx";
+import ScrollableTabs from "./components/2-nav-tabs/nav-tabs.jsx";
+import Hero from "./components/3-hero/hero.jsx";
+import About from "./components/4-about/about";
+
+//NPM MODULES
+import {
+  Link,
+  Button,
+  Element,
+  Events,
+  animateScroll as scroll,
+  scrollSpy,
+  scroller,
+} from "react-scroll";
+
+// Utils
+import { useIsInViewport } from "./utils/hooks";
+import { ClassSharp } from "@mui/icons-material";
 
 const App = () => {
+  const [value, setValue] = useState(0);
+
+  const activeTab = {
+    home: 0,
+    about: 1,
+    projects: 2,
+    skills: 3,
+  };
+
+  const handleSetActive = (e) => {
+    debugger;
+    setValue(activeTab[e]);
+  };
   return (
     <>
       <Heading />
-      <ScrollableTabs />
-      <Hero />
+
+      <ScrollableTabs onActive={handleSetActive} {...{ value }} />
+      <Element id="home" name="home">
+        <Hero />
+      </Element>
+
+      <Element id="about" name="about">
+        <About />
+      </Element>
+      <div style={{ background: "red", width: "100vw", height: "100vh" }}></div>
     </>
   );
 };
